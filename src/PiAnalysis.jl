ea2f4a46136ba597a28367e76c46f0a5ca299dfb
Courtney Allen <43384300+CourtA96@users.noreply.github.com>
2018-10-15 12:49:34 -0400

    Removed problem with chud() so it now has an accuracy of 10^-308.

--- src/PiAnalysis.jl
+++ src/PiAnalysis.jl
@@ -2,7 +2,7 @@ module PiAnalysis
 
 # using Plots
 
-# Number of digits in phi=n
+# Number of digits in pi = n
 n = 100
 
 setprecision(BigFloat,100000)
@@ -13,17 +13,17 @@ function chud()
 
   pInvTemp = 0
 
-  for i in 0:n
+  for i in 0:30
 
-    pInvTemp = pInvTemp + (-1)^(i)*factorial(BigInt(6*i))*BigInt(545140134*i+13591409)/((factorial(BigInt(3*i))*((factorial(BigInt(i)))^3)*(BigFloat(640320^(3*i+(3/2))))))
+    pInvTemp = pInvTemp + (-1)^(i)*factorial(BigInt(6*i))*BigInt(545140134*i+13591409)/((factorial(BigInt(3*i))*((factorial(BigInt(i)))^3)*(BigInt(640320)^BigFloat(3*i+(3/2)))))
 
   end
 
-  pInv = pInvTemp*12
+  pInv = BigFloat(pInvTemp*12)
 
   p = BigFloat(1/pInv)
 
-  return p
+  return BigFloat(p)
 
 end
 

7ed920d256091f6039ea1909b44e1fdda9409b4d
Courtney Allen <43384300+CourtA96@users.noreply.github.com>
2018-10-16 14:33:14 -0400

    Added test of chud() accuracy

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,11 +3,11 @@ using Test
 
 setprecision(BigFloat, 1024)
 
+# Check that chud() is calculating pi accurately to 100 decimal places
+# compare the result of chud() to that of Julia's pi
+
 p1 = BigFloat(pi)
 p2 = chud()
 diff = abs(p1-p2)
 
-
-@show diff
-@show p2
-@show numDig()
+@test diff < 10^(-100)

717dc1568c1610842b3c6abfe1d97574a707310b
Courtney Allen <43384300+CourtA96@users.noreply.github.com>
2018-10-18 08:17:30 -0400

    Added piAccurate() to compare pi to chud()

--- src/PiAnalysis.jl
+++ src/PiAnalysis.jl
@@ -2,18 +2,15 @@ module PiAnalysis
 
 using Plots
 
-# Number of digits in pi = n
-n = 100
-
-setprecision(BigFloat,100000)
-
 # Compute pi using the Chudnovsky algorithm
 export chud
-function chud()
+function chud(prcsn::Int)
+
+  setprecision(BigFloat,prcsn)
 
   pInvTemp = 0
 
-  for i in 0:30
+  for i in 0:50
 
     pInvTemp = pInvTemp + (-1)^(i)*factorial(BigInt(6*i))*BigInt(545140134*i+13591409)/((factorial(BigInt(3*i))*((factorial(BigInt(i)))^3)*(BigInt(640320)^BigFloat(3*i+(3/2)))))
 
@@ -27,8 +24,28 @@ function chud()
 
 end
 
+# Test Accuracy of Julia's pi
+
+export piAccurate
+function piAccurate(prcsn::Int)
+
+  setprecision(BigFloat,prcsn)
+
+  p1 = BigFloat(pi)
+  p2 = chud(prcsn)
+  diff = abs(p1-p2)
+
+  return diff
+
+end
+
+# Find recurrences of each digit of pi
+
 export numDig
 function numDig(a::Int,b::Int)
+
+  setprecision(BigFloat,2048)
+
   p = BigFloat(pi)
 
   temp = p*(10^BigFloat(b))
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,34 @@
 using PiAnalysis
 using Test
 
-setprecision(BigFloat, 1024)
-
 # Check that chud() is calculating pi accurately to 100 decimal places
-# compare the result of chud() to that of Julia's pi
+# by comparing the result of chud() to that of Julia's pi ( this also)
+# serves to confirm that Julia's pi is accurate.
 
-p1 = BigFloat(pi)
-p2 = chud()
-diff = abs(p1-p2)
+@testset "Test of chud()" begin
 
-@test diff < 10^(-100)
+  setprecision(BigFloat,2048)
+
+#  p1 = BigFloat(pi)
+#  p2 = chud(2048)
+#  diff = abs(p1-p2)
+
+  a_temp = 10^(BigInt(500))
+  a = BigFloat(1/a_temp)
+
+ diff = piAccurate(2048)
+
+  @test diff < a
+
+end
 
 @testset "Test of numDig()" begin
+
+  oneTo10_temp = numDig(1,10)
+  oneTo10 = oneTo10_temp[:,2]
+  correctOneTo10 = [0,2,1,1,1,3,1,0,0,1]
+
+  @test oneTo10-correctOneTo10 == [0,0,0,0,0,0,0,0,0,0]
 
   for i in 1:100
     i = 1

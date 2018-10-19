64d04206a6fbc7bf13da6623309a33512411779c
Courtney Allen <43384300+CourtA96@users.noreply.github.com>
2018-10-19 10:08:07 -0400

    Added comments

--- src/PiAnalysis.jl
+++ src/PiAnalysis.jl
@@ -2,11 +2,18 @@ module PiAnalysis
 
 using Plots
 
-# Compute pi using the Chudnovsky algorithm
+# Function to compute pi using the Chudnovsky algorithm
+#
+# chud() takes the desired precision of BigFloat as input
+
 export chud
 function chud(prcsn::Int)
 
+  # Set the precision of BigFloat using the input
+
   setprecision(BigFloat,prcsn)
+
+  # Begin the summation in the algorithm
 
   pInvTemp = 0
 
@@ -16,7 +23,11 @@ function chud(prcsn::Int)
 
   end
 
+  # End the summation and multiply the result by 12
+
   pInv = BigFloat(pInvTemp*12)
+
+  # Invert pInv to obtain pi
 
   p = BigFloat(1/pInv)
 
@@ -24,12 +35,20 @@ function chud(prcsn::Int)
 
 end
 
-# Test Accuracy of Julia's pi
+# Function to test the accuracy of Julia's pi by comparing it
+#
+# to the result of the Chudnovsky algorithm
+# piAccurate() takes the desired precision of BigFloat as input
 
 export piAccurate
 function piAccurate(prcsn::Int)
 
+  # Set the precision of BigFloat using the input
+
   setprecision(BigFloat,prcsn)
+
+  # Compute pi using Julia's value and chud() and return
+  # the absolute value of the difference
 
   p1 = BigFloat(pi)
   p2 = chud(prcsn)
@@ -39,10 +58,17 @@ function piAccurate(prcsn::Int)
 
 end
 
-# Find recurrences of each digit of pi
+# Function to find the number of recurrences of each digit of pi
+#
+# numDig() takes the number of the first and last digit in the
+# desired interval, as well as the desired precision of BigFloat,
+# as input
 
 export numDig
 function numDig(a::Int,b::Int,prcsn::Int)
+
+  # Return errors if the input interval doesn't make any sense,
+  # or if the precision of the desired interval is too small.
 
   if a>=b
     return error("The first input cannot be greater than or equal to the second input")
@@ -52,18 +78,31 @@ function numDig(a::Int,b::Int,prcsn::Int)
     return error("The precision must be higher for this interval (Try a precision that is at least 3.5 times the end bound)")
   end
 
+  # Set the precision of BigFloat using the third input
+
   setprecision(BigFloat,prcsn)
+
+  # Compute pi
 
   p = BigFloat(pi)
 
+  # Convert pi into an array of digits
+
   temp = p*(10^BigFloat(b))
   dig = Base.digits(trunc(BigInt,temp))
+
+  # Create an array to hold the numbers of recurrences the
+  # first column contains the digit, and the second contains
+  # the number of recurrences
+
   numDgts = zeros(Float64, 10, 2)
 
   for i in 1:10
     d = i-1
     numDgts[i,1] = d
   end
+
+  # Count the recurrences
 
   for i in a:b
 
@@ -92,12 +131,19 @@ function numDig(a::Int,b::Int,prcsn::Int)
     end
   end
 
+# return the array of digits
+
   return(numDgts)
 
 end
 
+# Function to compare the number of recurrences in two different
+# intervals
+
 export digCompare
 function digCompare(a::Int,b::Int,c::Int,d::Int,prcsn::Int)
+
+  # Return errors if the intervals are not equal or overlap
 
   if (b-a) != (d-c)
     return error("The intervals are not equal")
@@ -107,20 +153,39 @@ function digCompare(a::Int,b::Int,c::Int,d::Int,prcsn::Int)
     return error("The intervals cannot overlap")
   end
 
+  # Use numDig() to compute the recurrences in each interval
+
   dig1 = numDig(a,b,prcsn)
   dig2_temp = numDig(c,d,prcsn)
+
+  # Subtract out the first column of the second array so
+  # that the first column will remain when the arrays are
+  # subtracted
+
   dig2 = dig2_temp-[0 0;1 0;2 0;3 0;4 0;5 0;6 0;7 0;8 0;9 0]
+
+  # Return the result of subtracting the arrays
 
   return dig1-dig2
 
 end
 
+# Function to plot the recurrences as a pie chart
+#
+# Takes the number of the first and last digit in the
+# desired interval, as well as the desired precision of BigFloat
+# as input
+#
+# Uses Plots and pyplot()
+
 export piPlot
-function piPlot(a::Int,b::Int)
+function piPlot(a::Int,b::Int,prcsn::Int)
 
   pyplot()
 
-  values = numDig(a,b)
+  values = numDig(a,b,prcsn) # Compute the recurrences
+
+  # Put the values and the labels into two arrays and plot
 
   x = ["0","1","2","3","4","5","6","7","8","9"]
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,25 @@
 using PiAnalysis
 using Test
 
-# Check that chud() is calculating pi accurately to 100 decimal places
-# by comparing the result of chud() to that of Julia's pi ( this also)
-# serves to confirm that Julia's pi is accurate.
+# Check that chud() is calculating pi accurately by comparing the
+# result of chud() to that of Julia's pi then compares the
+# difference to the result of piAccurate
+#
+# Either they're both right or they're both wrong
 
 @testset "Test of chud() and piAccurate()" begin
 
+  # Check the accuracy of chud() and pi from 1024 bits
+  # to 102400 bits
+
   for i in 1:10
 
+    # Set the precision
+
     setprecision(BigFloat,1024*i)
+
+    # Compute the difference between pi and chud() and then
+    # compare to piAccurate()
 
     p1 = BigFloat(pi)
     p2 = chud(1024*i)
@@ -26,7 +36,13 @@ using Test
 
 end
 
+# Check that the first 10 digits of numDig() are correct and then
+# check that the results of numDig() and digCompare() don't vary
+# outside statistical probability
+
 @testset "Test of numDig()" begin
+
+  # Check that the first ten digits are correct
 
   oneTo10_temp = numDig(1,10,1024)
   oneTo10 = oneTo10_temp[:,2]
@@ -34,18 +50,27 @@ end
 
   @test oneTo10-correctOneTo10 == [0,0,0,0,0,0,0,0,0,0]
 
+  # Check that the results of numDig() and digCompare() don't vary
+  # outside statistical probability
+  #
+  # The digits of pi are randomly distributed, so the count for each
+  # digit shouldn't vary outside (+/-)sqrt(N) of 1/10th of N
+  # where N is the length of the interval. By the same reasoning,
+  # the result for each digit in digCompare() should be within
+  # (+/-)2*sqrt(N)
+
   for i in 1:100
-    i = 1
+
     ary1 = numDig(i,5*i,2048)
     ary2 = digCompare(i,2*i,2*i+1,3*i+1,2048)
     intvl = 5*i-i+1
     intvl2 = 2*i-i+1
 
     for j in 1:10
-      @test ary1[j,2] < (intvl/10) + sqrt(intvl)
+      @test ary1[j,2] < (intvl/10) + sqrt(intvl) # Test of numDig()
       @test ary1[j,2] > (intvl/10) - sqrt(intvl)
-      @test ary2[j,2] < sqrt(intvl2)
-      @test ary2[j,2] > -sqrt(intvl2)
+      @test ary2[j,2] < 2 * sqrt(intvl2)         # Test of digCompare()
+      @test ary2[j,2] > -2 * sqrt(intvl2)
     end
 
   end
